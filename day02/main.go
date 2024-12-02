eb0db3975ddf25031868faec4a42c3543cc14860
Alexander Rundberg <alex@gwai.se>
2024-12-02 16:34:04 +0100

    Scaffold solution for day02
    Also adds support for docker-compose file watch.

---
+++ day02/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"github.com/terminalnode/adventofcode2024/common"
+)
+
+func main() {
+	common.Setup(2, part1, part2)
+}
+
+func part1(input string) string {
+	return fmt.Sprintf("Not implemented yet")
+}
+
+func part2(input string) string {
+	return fmt.Sprintf("Not implemented yet")
+}
