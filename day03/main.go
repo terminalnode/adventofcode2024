01e3993ed739b5596a2e1eba4f91f95b7c273cc4
Alexander Rundberg <alex@gwai.se>
2024-12-03 12:41:30 +0100

    Scaffold solution for day03

---
+++ day03/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/terminalnode/adventofcode2024/common"
+)
+
+func main() {
+	common.Setup(2, part1, part2)
+}
+
+func part1(
+	input string,
+) string {
+	return "Not solved yet"
+}
+
+func part2(
+	input string,
+) string {
+	return "Not solved yet"
+}
