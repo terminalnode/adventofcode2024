00992a20d93c8985f9eba5c9a3e85b41d215faa8
Alexander Rundberg <alex@gwai.se>
2024-12-04 15:06:16 +0100

    Update client.sh to use kubernetes or docker-compose traefik
    If minikube is installed, default to using $(minikube ip) as the
    backend, otherwise use localhost. This can be overridden by the user
    providing their own URL.

--- client.sh
+++ client.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 # Simple script to build and run the client module without having to produce
 # a binary file
+profile_name='aoc2024'
+url='localhost'
+
+if [ -n "$(command -v minikube)" ] && minikube profile list | grep -q "$profile_name"; then
+  echo "Minikube is installed and profile '$profile_name' exists, using minikube ip ($(minikube ip))."
+  echo "Override this by providing your own -url to this command."
+  url="$(minikube ip)"
+else
+  echo "Minikube is not installed or profile '$profile_name' doesn't exist, using localhost."
+fi
 
 # shellcheck disable=SC2068
-go run ./client $@
+go run ./client -url "$url" $@
