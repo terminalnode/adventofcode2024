8df7ede9f3b00b84eae15a09d6b5959be476a784
Alexander Rundberg <alex@gwai.se>
2024-12-11 22:19:50 +0100

    Add solution day 11 part 2

--- solutions/day11/main.go
+++ solutions/day11/main.go
@@ -7,28 +7,40 @@ import (
 	"strings"
 )
 
+// Stone => blinks remaining => output
+type blinkCache = map[int]map[int]int
+
 func main() {
-	common.Setup(11, part1, nil)
+	common.Setup(11, part1, part2)
 }
 
-func part1(
+func part1(input string) string { return solve(input, 25) }
+
+func part2(input string) string { return solve(input, 75) }
+
+func solve(
 	input string,
+	blinks int,
 ) string {
 	stones, err := parseStones(input)
-	lenStart := len(stones)
-
+	cache := make(blinkCache)
 	if err != nil {
 		return fmt.Sprintf("Failed to parse stones: %v", err)
 	}
+	lenStart := len(stones)
 
-	for i := 0; i < 25; i++ {
-		stones, err = blink(stones)
+	out := 0
+	for i, stone := range stones {
+		result, err := blink(stone, blinks, cache)
 		if err != nil {
-			return fmt.Sprintf("Run failed on blink #%d: %v", i+1, err)
+			return fmt.Sprintf("Failed to blink stone #%d (%d): %v", i+1, stone, err)
 		}
+
+		out += result
+		fmt.Printf("Stone with value %d resulted in %d (out = %d)\n", stone, result, out)
 	}
 
-	return fmt.Sprintf("%d stones have turned into %d after 25 blinks", lenStart, len(stones))
+	return fmt.Sprintf("%d stones have turned into %d after %d blinks", lenStart, out, blinks)
 }
 
 func parseStones(
@@ -50,26 +62,75 @@ func parseStones(
 }
 
 func blink(
-	stones []int,
-) ([]int, error) {
-	// Initialize the out-array with twice the size to ensure that we never need to resize it
-	out := make([]int, 0, 2*len(stones))
+	stone int,
+	blinksLeft int,
+	cache blinkCache,
+) (int, error) {
+	var out int
+	var err error
 
-	for _, stone := range stones {
-		if stone == 0 {
-			out = append(out, 1)
-		} else if s := strconv.Itoa(stone); len(s)%2 == 0 {
-			left, right, err := splitNumber(s)
-			if err != nil {
-				return out, err
-			}
-			out = append(out, left, right)
-		} else {
-			out = append(out, stone*2024)
-		}
+	// Early exit if this value has already been computed.
+	out = getCacheValueOrBaseCase(cache, stone, blinksLeft)
+	if out != 0 {
+		return out, err
 	}
 
+	// Apply the three rules
+	if stone == 0 {
+		out, err = blink(1, blinksLeft-1, cache)
+	} else if s := strconv.Itoa(stone); len(s)%2 == 0 {
+		out, err = blinkSplit(s, blinksLeft-1, cache)
+	} else {
+		out, err = blink(stone*2024, blinksLeft-1, cache)
+	}
+
+	// Exit on error
+	if err != nil {
+		return 0, err
+	}
+
+	// Enter value into the cache and return
+	cache[stone][blinksLeft] = out
 	return out, nil
+}
+
+func getCacheValueOrBaseCase(
+	cache blinkCache,
+	stone int,
+	blinksLeft int,
+) int {
+	if blinksLeft == 0 {
+		return 1
+	}
+
+	if cache[stone] == nil {
+		cache[stone] = make(map[int]int)
+	}
+
+	return cache[stone][blinksLeft]
+}
+
+func blinkSplit(
+	stone string,
+	blinks int,
+	cache blinkCache,
+) (int, error) {
+	left, right, err := splitNumber(stone)
+	if err != nil {
+		return 0, err
+	}
+
+	leftValue, err := blink(left, blinks, cache)
+	if err != nil {
+		return 0, err
+	}
+
+	rightValue, err := blink(right, blinks, cache)
+	if err != nil {
+		return 0, err
+	}
+
+	return leftValue + rightValue, nil
 }
 
 func splitNumber(
