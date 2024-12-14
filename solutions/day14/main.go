e96bdb5bb2af07b80d5198e857575c6c7b8e36c1
Alexander Rundberg <alex@gwai.se>
2024-12-14 17:10:56 +0100

    Refactor solution day 14 part 1

--- solutions/day14/main.go
+++ solutions/day14/main.go
@@ -3,7 +3,13 @@ package main
 import (
 	"fmt"
 	"github.com/terminalnode/adventofcode2024/common"
+	"github.com/terminalnode/adventofcode2024/common/util"
 )
+
+const maxX = 100
+const maxY = 102
+const horizontalLine = maxY / 2
+const verticalLine = maxX / 2
 
 func main() {
 	common.Setup(14, part1, nil)
@@ -12,44 +18,40 @@ func main() {
 func part1(
 	input string,
 ) string {
-	return solve(input, 100, 100, 102)
-}
-
-func solve(
-	input string,
-	seconds int,
-	maxX int,
-	maxY int,
-) string {
-	// Lines assume that the board is an uneven number of tiles
-	horizontalLine := maxY / 2
-	verticalLine := maxX / 2
-
 	robots, err := parseRobots(input)
 	if err != nil {
 		return fmt.Sprintf("Failed to parse robots: %v", err)
 	}
 
 	q1, q2, q3, q4 := 0, 0, 0, 0
-	for _, r := range robots {
-		move := r.move.Multiply(seconds, seconds)
-		newPos := r.init.Add(move.X, move.Y).PositiveModulo(maxX+1, maxY+1)
-
-		if newPos.X < verticalLine {
-			if newPos.Y < horizontalLine {
+	for _, p := range getNewPositions(robots, 100) {
+		if p.X < verticalLine {
+			if p.Y < horizontalLine {
 				q1++
-			} else if newPos.Y > horizontalLine {
+			} else if p.Y > horizontalLine {
 				q2++
 			}
-		} else if newPos.X > verticalLine {
-			if newPos.Y < horizontalLine {
+		} else if p.X > verticalLine {
+			if p.Y < horizontalLine {
 				q3++
-			} else if newPos.Y > horizontalLine {
+			} else if p.Y > horizontalLine {
 				q4++
 			}
 		}
 	}
 
-	safetyFactor := q1 * q2 * q3 * q4
-	return fmt.Sprintf("After 100 seconds, area has a safety factor of %d", safetyFactor)
+	return fmt.Sprintf("After 100 seconds, area has a safety factor of %d", q1*q2*q3*q4)
+}
+
+func getNewPositions(
+	robots []robot,
+	seconds int,
+) []util.Coordinate {
+	out := make([]util.Coordinate, len(robots))
+	for i, r := range robots {
+		move := r.move.Multiply(seconds, seconds)
+		out[i] = r.init.Add(move.X, move.Y).PositiveModulo(maxX+1, maxY+1)
+
+	}
+	return out
 }
